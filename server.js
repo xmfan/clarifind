d96a8cb4f1ae8a7bea488f5b441fce6e47008be3
Simon Fan <simon.fan@metricwire.com>
2016-02-27 04:35:56 -0500

    auto auth

--- server.js
+++ server.js
@@ -3,37 +3,29 @@
 var config = require('./config.js'),
     express = require('express'),
     app = express(),
-    request = require('request'),
-    access_token;
-
-requestAccessToken();
+    request = require('request');
 
 app.get('/', function(req, res) {
     var options = {
       url: 'https://api.clarifai.com/v1/color/',
       headers: {
-        'Authorization': 'Bearer ' + config.access_token
+        'Authorization': 'Bearer ' + global.access_token
       }
     };
-    console.log(access_token);
+
     function callback(error, response, body) {
         if (error) console.log(error);
         else if (response.statusCode == 401) {
-            config.requestAccessToken();
+            config.requestAccessToken;
+            request(options, callback);
+        } else {
+            res.send(body);
         }
-        request(options, callback);
     }
+    request(options, callback);
 });
 
 
 app.listen(4000, function() {
     console.log('Server running on: 4000');
 });
-
-function requestAccessToken() {
-    request.post({url:'https://api.clarifai.com/v1/token/', form: config.header}, function(error, response, body){
-        if (error) console.log(error);
-        else access_token = JSON.parse(body).access_token;
-        console.log(config.header);
-    });
-}
