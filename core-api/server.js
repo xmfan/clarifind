dc09e52e09a9548c8e918b0b3718e3ac42d2d46a
Simon Fan <simon.fan@metricwire.com>
2016-02-27 16:19:16 -0500

    clarifai integrated

--- core-api/server.js
+++ core-api/server.js
@@ -3,14 +3,16 @@
 var config = require('./config.js'),
     express = require('express'),
     app = express(),
-    request = require('request');
+    request = require('request'),
+    curl = require('node-curl');
+    
 
 app.get('/', function(req, res) {
     var options = {
-      url: 'https://api.clarifai.com/v1/color/',
-      headers: {
-        'Authorization': 'Bearer ' + global.access_token
-      }
+        url: 'https://api.clarifai.com/v1/color/',
+        headers: {
+            'Authorization': 'Bearer ' + global.access_token
+        }
     };
 
     function callback(error, response, body) {
@@ -26,6 +28,35 @@ app.get('/', function(req, res) {
     request(options, callback);
 });
 
+app.post('/tag', function(req, res) {
+    var options = {
+        url: 'https://api.clarifai.com/v1/tag',
+        headers: {
+            'Authorization': 'Bearer ' + global.access_token
+        }, 
+        formData: {
+            url: ['http://www.clarifai.com/img/metro-north.jpg', 'http://www.clarifai.com/img/metro-north.jpg']
+        }
+    };
+
+    function callback(error, response, body) {
+        if (error) console.log(error);
+        else if (response.statusCode == 401) {
+            res.sendStatus(body);
+            //config.requestAccessToken;
+            //request(options, callback);
+        } else {
+            res.send(JSON.parse(body));
+        }
+    }
+    
+    request.post(options, callback);
+});
+
+app.get('/android', function(req, res) {
+    console.log('android endpoint');
+    res.send('android endpoint reached');
+});
 
 app.listen(4000, function() {
     console.log('Server running on: 4000');
