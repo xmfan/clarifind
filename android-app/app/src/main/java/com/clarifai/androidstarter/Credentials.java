ac5162495302c52b7e1256a4d27c82398cdfa055
Matthew Tse <mtse9779@gmail.com>
2016-02-28 10:57:18 -0500

    new files

---
+++ android-app/app/src/main/java/com/clarifai/androidstarter/Credentials.java
@@ -0,0 +1,13 @@
+package com.clarifai.androidstarter;
+
+class Credentials {
+  // IMPORTANT NOTE: you should replace the CLIENT_ID and CLIENT_SECRET values below
+  // with your own. You can obtain these at https://developer.clarifai.com/applications -- the
+  // defaults are shared credentials that will stop working once their quota is reached.
+  //
+  // Also, if you're committing your code to a public repo, be careful not to check in your
+  // credentials -- we're including these here so you can get started out of the box, but this is
+  // generally a bad practice.
+  static final String CLIENT_ID = "NS0-lMrU85GPaUsgGB0V2jm8HlteI-t72Bp2Wy0H";
+  static final String CLIENT_SECRET = "FfAFqCI6lpE7CPryWfPjWrP81prh5XKOZBgH4-fU";
+}
---
+++ android-app/app/src/main/java/com/clarifai/androidstarter/RestService.java
@@ -0,0 +1,12 @@
+package com.clarifai.androidstarter;
+
+/**
+ * Created by mcotse on 2016-02-27.
+ */
+//public class RestService {
+//    public static final String API_ENDPOINT = "http://35.14.149.201:4000";
+//
+//    @POST("/android")
+//    void test(
+//            Callback<String> cb);
+//}
